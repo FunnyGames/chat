5abc99b1147cff03b5b6b7f20b9fb0271dfa21c6
Dmytro Zagorodny <fgmmaker@gmail.com>
2020-01-21 01:49:30 +0200

    Removed cors

--- server/src/startup/middlewares.js
+++ server/src/startup/middlewares.js
@@ -8,14 +8,9 @@ var cors = require('cors');
 // This will configure all middlewares
 module.exports = (app) => {
     // This allows for any hostname to connect
-    var whitelist = ['http://localhost:3000', 'http://3.122.233.242:8080']
     var corsOptions = {
         origin: function (origin, callback) {
-            if (whitelist.indexOf(origin) !== -1) {
-                callback(null, true)
-            } else {
-                callback(new Error('Not allowed by CORS'))
-            }
+            callback(null, true);
         }
     }
     app.use(cors(corsOptions));
